f646b47f47e90d2c0a80e1d1779889963d2438cf
rickyars <rickyars@gmail.com>
2026-05-14 22:53:14 -0500

    feat: extract firebase config to separate file, fix manifest recursion, add firebase hosting config

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey:            "REPLACE_ME",
+  authDomain:        "REPLACE_ME.firebaseapp.com",
+  databaseURL:       "https://REPLACE_ME-default-rtdb.firebaseio.com",
+  projectId:         "REPLACE_ME",
+  storageBucket:     "REPLACE_ME.appspot.com",
+  messagingSenderId: "REPLACE_ME",
+  appId:             "REPLACE_ME"
+};
--- generate-manifest.js
+++ generate-manifest.js
@@ -5,9 +5,20 @@ const IMAGES_DIR = path.join(__dirname, 'images');
 const OUT_FILE   = path.join(__dirname, 'images.json');
 const ACCEPTED   = new Set(['jpg', 'jpeg', 'png', 'gif', 'webp']);
 
-const files = fs.readdirSync(IMAGES_DIR)
-  .filter(f => ACCEPTED.has(f.split('.').pop().toLowerCase()))
-  .sort();
+function scan(dir, base) {
+  const entries = fs.readdirSync(dir, { withFileTypes: true });
+  let files = [];
+  for (const entry of entries) {
+    if (entry.isDirectory()) {
+      files = files.concat(scan(path.join(dir, entry.name), path.join(base, entry.name)));
+    } else if (ACCEPTED.has(entry.name.split('.').pop().toLowerCase())) {
+      files.push(path.join(base, entry.name).replace(/\\/g, '/'));
+    }
+  }
+  return files;
+}
+
+const files = scan(IMAGES_DIR, '').sort();
 
 fs.writeFileSync(OUT_FILE, JSON.stringify(files, null, 2));
 console.log(`Written ${files.length} entries to images.json`);
